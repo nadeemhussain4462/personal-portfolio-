d284eb7b1e2879b6261c153a596ad7373cddd7e5
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-10-29 23:42:49 +0500

    Done my portfolio

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const hamburger = document.getElementById('hamburger');
 const list = document.getElementById('list');
+const bars = document.getElementById('bars');
 
 hamburger.addEventListener('click', ()=>{
     list.classList.toggle('navlist-active');
+    bars.classList.toggle('fa-xmark');
 });
